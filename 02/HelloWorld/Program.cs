8b5c4115e6ec5661c683c0dcfaddb6e02efa509c
Pankaj Rayal <https://github.dxc.com/>
2021-04-09 19:27:03 +0530

    02-Getting Started

---
+++ 02/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(JsonConvert.SerializeObject(args));
+        }
+    }
+}
