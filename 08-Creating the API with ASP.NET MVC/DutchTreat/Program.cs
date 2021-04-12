355e38b804cea0f5dfa385806f7968721f29dbaf
Pankaj Rayal <https://github.dxc.com/>
2021-04-12 21:02:35 +0530

    08-Creating the API with ASP.NET MVC

---
+++ 08-Creating the API with ASP.NET MVC/DutchTreat/Program.cs
@@ -0,0 +1,44 @@
+using DutchTreat.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+namespace DutchTreat {
+    public class Program {
+        public static void Main(string[] args) {
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            if (args.Length > 0 && args[0].ToLower() == "/seed") {
+                RunSeeding(host);
+                return;
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(AddConfiguration)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void AddConfiguration(HostBuilderContext ctx, IConfigurationBuilder bldr) {
+            bldr.Sources.Clear();
+            bldr.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+        }
+
+        public static void RunSeeding(IHost host) {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope()) {
+                var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+                seeder.Seed();
+            }
+        }
+    }
+}
---
+++ 08-Creating the API with ASP.NET MVC/DutchTreat/Services/IMailService.cs
@@ -0,0 +1,5 @@
+﻿namespace DutchTreat.Services {
+    public interface IMailService {
+        void SendMessage(string to, string subject, string body);
+    }
+}
