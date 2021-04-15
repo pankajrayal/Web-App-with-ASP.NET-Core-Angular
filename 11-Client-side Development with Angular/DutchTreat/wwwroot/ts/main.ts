f315306ccc753e2a8f0451d310c2841eaccfc857
Pankaj Rayal <https://github.dxc.com/>
2021-04-15 20:33:02 +0530

    11-Client-side Development with Angular

---
+++ 11-Client-side Development with Angular/DutchTreat/wwwroot/ts/main.ts
@@ -0,0 +1,4 @@
+﻿//wwwroot\ts\main.ts
+//let shopper = new Shopper("Pankaj", 4);
+const shopper = new Shopper("Pankaj", "Rayal");
+shopper.showName();
---
+++ 11-Client-side Development with Angular/DutchTreat/wwwroot/ts/shopper.js
@@ -0,0 +1,12 @@
+//DutchTreat\wwwroot\ts\shopper.ts
+var Shopper = /** @class */ (function () {
+    function Shopper(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+    Shopper.prototype.showName = function () {
+        alert(this.first + " " + this.last);
+    };
+    return Shopper;
+}());
+//# sourceMappingURL=shopper.js.map
